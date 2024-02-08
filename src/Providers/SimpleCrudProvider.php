3fa63aca740dcd947442aeb6f77aa4141c650faa
sureshma <suresh.a@globalintegra.net>
2024-02-08 16:15:42 +0530

    fixed pakcage name changes

--- src/Providers/SimpleCrudProvider.php
+++ src/Providers/SimpleCrudProvider.php
@@ -15,7 +15,6 @@ class SimpleCrudProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/../views', 'simple-crud-builder');
         Blade::componentNamespace("Masuresh124\\SimpleCrudBuilder\\View\\Components", 'simple-crud-builder');
     }
--- src/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Masuresh124\SimpleCrudBuilder\Controllers\SimpleCrudController;
-
-Route::get('simple-crud-builder', SimpleCrudController::class);
